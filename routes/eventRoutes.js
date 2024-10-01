2a7b6f2a1ac26ccc842a7672fd9106ed6a22b0f7
Chris Azuaje <christopherazuaje@gmail.com>
2024-10-01 16:48:41 -0400

    Update to MVC Format

---
+++ routes/eventRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// GET /events: send all events to the user
+router.get('/', (req, res) => {
+  res.render('events');
+});
+
+// GET /events/new: send html form for creating a new event
+router.get('/newEvent', (req, res) => {
+  res.render('newEvent');
+});
+
+// GET /events/event: send individual event to user
+router.get('/event', (req, res) => {
+  res.render('event');
+});
+
+module.exports = router;
